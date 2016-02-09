466a77eeec3d579530431f980794f87e29c763c2
bradhilton <brad.hilton.nw@gmail.com>
2016-02-09 09:35:04 -0700

    Initial Commit

---
+++ PropertyExtensions/PropertyExtensions.h
@@ -0,0 +1,19 @@
+//
+//  PropertyExtensions.h
+//  PropertyExtensions
+//
+//  Created by Bradley Hilton on 2/9/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PropertyExtensions.
+FOUNDATION_EXPORT double PropertyExtensionsVersionNumber;
+
+//! Project version string for PropertyExtensions.
+FOUNDATION_EXPORT const unsigned char PropertyExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PropertyExtensions/PublicHeader.h>
+
+
